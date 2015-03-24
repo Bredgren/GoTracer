946ccb43df927573739991b989d20b3d8323214d
Bredgren <brandon.edgren@gmail.com>
2015-03-23 19:19:38 -0700

    Split sphere into separate file.

--- object_test.go
+++ object_test.go
@@ -1,7 +1,6 @@
 package gotracer
 
 import (
-	"math/rand"
 	"testing"
 
 	"github.com/go-gl/mathgl/mgl64"
@@ -28,91 +27,6 @@ func testIsect(t *testing.T, desc string, itr Intersecter, ray Ray, exp Expected
 	if (exp.isect.Object != nil && !isectEqual(isect, exp.isect)) || hit != exp.hit {
 		actual := ExpectedIsect{isect, hit}
 		t.Errorf("%v: Incorrect intersection. Expected %v, got %v", desc, exp, actual)
-	}
-}
-
-func TestSphereIntersect(t *testing.T) {
-	obj := NewObject(mgl64.Ident4(), nil)
-	sphere := Sphere{obj}
-
-	ray := NewRay(PrimaryRay, mgl64.Vec3{0, 0, 5}, mgl64.Vec3{0, 0, -1})
-	exp := ExpectedIsect{Intersection{obj, mgl64.Vec3{0, 0, 1}, 4, mgl64.Vec2{0.5, 0}}, true}
-	testIsect(t, "Origin", sphere, ray, exp)
-
-	ray.Origin = mgl64.Vec3{0, 0, -5}
-	exp.hit = false
-	testIsect(t, "Behind", sphere, ray, exp)
-
-	ray.Origin = mgl64.Vec3{-(1 - Rayε), 0, 5}
-	exp.isect.Object = nil
-	exp.hit = true
-	testIsect(t, "Left graze hit", sphere, ray, exp)
-	ray.Origin = mgl64.Vec3{-1, 0, 0}
-	exp.hit = false
-	testIsect(t, "Left graze no hit", sphere, ray, exp)
-
-	ray.Origin = mgl64.Vec3{1 - Rayε, 0, 5}
-	exp.hit = true
-	testIsect(t, "Right graze hit", sphere, ray, exp)
-	ray.Origin = mgl64.Vec3{1, 0, 0}
-	exp.hit = false
-	testIsect(t, "Right graze no hit", sphere, ray, exp)
-
-	ray.Origin = mgl64.Vec3{0, 0, 0}
-	exp.isect.Object = obj
-	exp.isect.Normal = mgl64.Vec3{0, 0, -1}
-	exp.isect.T = 1
-	exp.isect.UVCoords = mgl64.Vec2{0.5, 1}
-	exp.hit = true
-	testIsect(t, "Inside", sphere, ray, exp)
-
-	ray.Origin = mgl64.Vec3{0, 0, 1}
-	exp.isect.Object = obj
-	exp.isect.Normal = mgl64.Vec3{0, 0, -1}
-	exp.isect.T = 2
-	exp.isect.UVCoords = mgl64.Vec2{0.5, 1}
-	testIsect(t, "Touching front", sphere, ray, exp)
-	ray.Origin = mgl64.Vec3{0, 0, 1 + Rayε}
-	exp.isect.Normal = mgl64.Vec3{0, 0, 1}
-	exp.isect.T = Rayε
-	exp.isect.UVCoords = mgl64.Vec2{0.5, 0}
-	testIsect(t, "Close to front", sphere, ray, exp)
-	ray.Origin = mgl64.Vec3{0, 0, 1 - Rayε}
-	exp.isect.Normal = mgl64.Vec3{0, 0, -1}
-	exp.isect.T = 2 - Rayε
-	exp.isect.UVCoords = mgl64.Vec2{0.5, 1}
-	testIsect(t, "Close inside front", sphere, ray, exp)
-	ray.Origin = mgl64.Vec3{0, 0, -1}
-	exp.hit = false
-	testIsect(t, "Touching back", sphere, ray, exp)
-}
-
-func BenchmarkSphereIntersect(b *testing.B) {
-	sphere := Sphere{NewObject(mgl64.Ident4(), nil)}
-
-	ray := NewRay(PrimaryRay, mgl64.Vec3{0, 0, 5}, mgl64.Vec3{0, 0, -1})
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		sphere.Intersect(ray)
-	}
-}
-
-func BenchmarkSphereIntersectRandom(b *testing.B) {
-	sphere := Sphere{NewObject(mgl64.Ident4(), nil)}
-
-	min := -1.0
-	max := 1.0
-	rays := make([]Ray, 0)
-	for i := 0; i < b.N; i++ {
-		x := rand.Float64()*(max-min) - max
-		y := rand.Float64()*(max-min) - max
-		ray := NewRay(PrimaryRay, mgl64.Vec3{x, y, 5}, mgl64.Vec3{0, 0, -1})
-		rays = append(rays, ray)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		sphere.Intersect(rays[i])
 	}
 }
 
---
+++ sphere.go
@@ -0,0 +1,54 @@
+package gotracer
+
+import (
+	"math"
+
+	"github.com/go-gl/mathgl/mgl64"
+)
+
+type Sphere struct {
+	Object *Object
+}
+
+func (s Sphere) Intersect(r Ray) (isect Intersection, hit bool) {
+	isect = Intersection{Object: s.Object}
+
+	// -(d . o) +- sqrt((d . o)^2 - (d . d)((o . o) - 1)) / (d . d)
+	do := r.Dir.Dot(r.Origin)
+	dd := r.Dir.Dot(r.Dir)
+	oo := r.Origin.Dot(r.Origin)
+
+	discriminant := do*do - dd*(oo-1)
+	if discriminant < 0 {
+		return isect, false
+	}
+
+	discriminant = math.Sqrt(discriminant)
+
+	t2 := (-do + discriminant) / dd
+	if t2 <= Rayε {
+		return isect, false
+	}
+
+	t1 := (-do - discriminant) / dd
+	if t1 > Rayε {
+		isect.T = t1
+		// No need to normalize because it's a unit sphere at the origin
+		isect.Normal = r.At(t1)
+		u := 0.5 + (math.Atan2(isect.Normal.Y(), isect.Normal.X()) / (2 * math.Pi))
+		v := 0.5 - (math.Asin(isect.Normal.Z()) / math.Pi)
+		isect.UVCoords = mgl64.Vec2{u, v}
+		return isect, true
+	}
+
+	if t2 > Rayε {
+		isect.T = t2
+		isect.Normal = r.At(t2)
+		u := 0.5 + (math.Atan2(isect.Normal.Y(), isect.Normal.X()) / (2 * math.Pi))
+		v := 0.5 - (math.Asin(isect.Normal.Z()) / math.Pi)
+		isect.UVCoords = mgl64.Vec2{u, v}
+		return isect, true
+	}
+
+	return isect, false
+}
