313275c42cea8f93e7dd56bffc713c7296ea23de
Brandon Edgren <brandon.edgren@gmail.com>
2015-11-26 12:06:35 -0800

    Finish converting to gohtmlctrl. Clean up code and the UI.

--- lib/options.go
+++ lib/options.go
@@ -2,145 +2,176 @@ package lib
 
 // Options contains all the options possible
 type Options struct {
-	Resolution struct {
-		W, H int `min:"1" max:"16000" default:"600"`
-	} `desc:"Image width and height in pixels"`
-	Camera struct {
-		Position Vector `desc:"Camera position"`
-		LookAt   Vector `desc:"The point that the camera is looking at"`
-		UpDir    Vector `desc:"The up direction"`
-		Fov      Angle  `desc:"Field of view"`
-	} `desc:"Camera position and orientation"`
-	Background struct {
-		Type  string `desc:"Type of background" Types:"Uniform,Skybox" default:"Uniform"`
-		Color Color  `desc:"Color to use for a solid background" Type:"Uniform"`
-		Image string `desc:"Path/URL of image to use as skybox" Type:"Skybox"`
-	} `desc:"Determines the color when a ray doesn't hit anything"`
-	AntiAlias struct {
-		MaxDivisions int     `desc:"Maximum subdivisions of a pixel" min:"0" max:"16" default:"0"`
-		Threshold    float64 `desc:"Stop subdividing pixels when the difference is less than this" min:"0.0" max:"99"`
-	} `desc:"Anti-aliasing settings"`
-	MaxRecursion int         `desc:"Maximum reflective and refractive rays per pixel" min:"0" max:"99" default:"2"`
-	SoftShadows  int         `desc:"Soft shadow detail. 0 disables soft shadows" min:"0" max:"99" default:"0"`
-	Lights       []*Light    `desc:"List of all lights in the scene"`
-	Materials    []*Material `desc:"List of all the materials available to objects"`
-	Objects      []*Object   `dest:"List of all objects in the scene"`
+	Global     global      `title:"Global options"`
+	Resolution resolution  `title:"Image width and height in pixels"`
+	Background background  `title:"Determines the color when a ray doesn't hit anything"`
+	Camera     camera      `title:"Camera position and orientation"`
+	AntiAlias  antiAlias   `title:"Anti-aliasing settings"`
+	Lights     []*light    `title:"List of all lights in the scene"`
+	Materials  []*material `title:"List of all the materials available to objects"`
+	Objects    []*object   `title:"List of all objects in the scene"`
 }
 
-// Light is a light source in the scene.
-type Light struct {
-	Type      string `desc:"Type of light" Types:"Directional,Point,Spot"`
-	Color     Color  `desc:"Color of the light"`
-	Position  Vector `desc:"Position of the light"`
-	Direction Vector `desc:"Direction of the light" Type:"Directional,Spot"`
+type global struct {
+	FastRender       bool `title:"Disable/limit some settings in order to decrease rendering time"`
+	MaxRecursion     int  `title:"Maximum reflective/refractive rays per pixel" min:"0" max:"99"`
+	SoftShadowDetail int  `title:"Soft shadow detail. 0 disables soft shadows" min:"0" max:"99"`
+}
+
+type resolution struct {
+	W int `title:"Width in pixels" min:"1" max:"16000"`
+	H int `title:"Height in pixels" min:"1" max:"16000"`
+}
+
+type camera struct {
+	Position vector  `title:"Camera position"`
+	LookAt   vector  `title:"The point that the camera is looking at"`
+	UpDir    vector  `title:"The up direction"`
+	Fov      float64 `title:"Field of view in degrees" min:"0.0" max:"360.0"`
+}
+
+type background struct {
+	Type  string `title:"Type of background" choice:"Uniform,Skybox"`
+	Color color  `title:"Color to use for a solid background (Uniform)"`
+	Image string `title:"Path/URL of image to use (Skybox)"`
+}
+
+type antiAlias struct {
+	MaxDivisions int     `title:"Maximum subdivisions of a pixel" min:"0" max:"16"`
+	Threshold    float64 `title:"Stop subdividing pixels when the difference is less than this" min:"0.0" max:"99"`
+}
+
+type light struct {
+	Type      string `title:"Type of light" choice:"Directional,Point,Spot"`
+	Color     color  `title:"Color of the light"`
+	Position  vector `title:"Position of the light (Point and Spot)"`
+	Direction vector `title:"Direction of the light (Direcitonal and Spot)"`
 	Coeff     struct {
 		Constant  float64
 		Linear    float64
 		Quadratic float64
-	} `desc:"Constant, lnear, and quadratic coefficients for point light fall off" Type:"Point"`
-	Angle     float64 `desc:"Spotlight angle" Type:"Spot"`
-	DropOff   float64 `desc:"Spotlight drop off angle" Type:"Spot"`
-	FadeAngle float64 `desc:"Stoplight fade angle" Type:"Spot"`
+	} `title:"Constant, lnear, and quadratic coefficients for point light fall off (Point)"`
+	Angle     float64 `title:"Spotlight angle" Type:"Spot" min:"0.0" max:"360.0"`
+	DropOff   float64 `title:"Spotlight drop off angle" Type:"Spot" min:"0.0" max:"360.0"`
+	FadeAngle float64 `title:"Stoplight fade angle" Type:"Spot" min:"0.0" max:"360.0"`
 }
 
-// Material is a description of a material that can be used by Objects.
-type Material struct {
-	Name         string      `desc:"Unique name for the material"`
-	Parent       string      `desc:"Name of existing material to inherit attributes from"`
-	Emissive     MatProperty `desc:"Emissive color of the material"`
-	Ambient      MatProperty `desc:"Abmient color of the material"`
-	Diffuse      MatProperty `desc:"Diffuse color of the material"`
-	Specular     MatProperty `desc:"Specular color of the material"`
-	Reflective   MatProperty `desc:"Reflectivness of the material"`
-	Transmissive MatProperty `desc:"Transmissivness of the material"`
-	Smoothness   MatProperty `desc:"Smoothness of the material. Affects size of speclar spots"`
-	Index        float64     `desc:"Refractive index of the material"`
-	Normal       string      `desc:"Path/URL of image to use as a normal map"`
-	IsLiquid     bool        `desc:"Overlapping behavior is only defined for non-liquids inside liquids"`
-	Brdf         string      `desc:"Lambert, Blinn-Phong"`
+type material struct {
+	Name         string      `title:"Unique name for this material"`
+	Parent       string      `title:"Name of existing material to inherit attributes from"`
+	Emissive     matProperty `title:"Emissive color of the material"`
+	Ambient      matProperty `title:"Abmient color of the material"`
+	Diffuse      matProperty `title:"Diffuse color of the material"`
+	Specular     matProperty `title:"Specular color of the material"`
+	Reflective   matProperty `title:"Reflectivness of the material"`
+	Transmissive matProperty `title:"Transmissivness of the material"`
+	Smoothness   matProperty `title:"Smoothness of the material. Affects size of speclar spots"`
+	Index        float64     `title:"Refractive index of the material"`
+	Normal       string      `title:"Path/URL of image to use as a normal map"`
+	IsLiquid     bool        `title:"Overlapping behavior is only defined for non-liquids inside liquids"`
+	Brdf         string      `title:"Shadding algorithm" choice:"Lambert,Blinn-Phong"`
 }
 
-// Object is a rendreable object.
-type Object struct {
-	Type         string   `desc:"Type of shape" Types:"Transform,Sphere,Box,Square,Cylinder,Cone"`
-	Translate    Vector   `desc:"Transform translation" Type:"Transform"`
-	RotateAxis   Vector   `desc:"Transform axis to rotate around" Type:"Transform"`
-	RotateAngle  Angle    `desc:"Transform angle to rotate around the axis" Type:"Transform"`
-	Scale        Vector   `desc:"Transform scale" Type:"Transform"`
-	SubObjects   []Object `desc:"Objects to apply the transform to" Type:"Transform"`
-	Material     string   `desc:"Name of the material to use" Type:"Sphere,Box,Square,Cylinder,Cone"`
-	TopRadius    float64  `desc:"Top radius of the cone" Type:"Cone"`
-	BottomRadius float64  `desc:"Bottom radius of the cone" Type:"Cone"`
-	Capped       bool     `desc:"Whether to cap the cone/cylinder" Type:"Cynlinder,Cone"`
+type matProperty struct {
+	Type    string `title:"Type of material" choice:"Uniform,Texture"`
+	Color   color  `title:"Uniform color"`
+	Texture string `title:"Path to texture file"`
 }
 
-// MatProperty is a Material property which can either be a texture or a uniform color
-type MatProperty struct {
-	Type    string `desc:"Type of material" Types:"Uniform,Texture"`
-	Texture string `desc:"Path to texture file" Type:"Texture"`
-	Color   Color  `desc:"Uniform color" Type:"Uniform"`
+type object struct {
+	Type         string    `title:"Type of shape. The 'Transform' type is an invisible object" choice:"Transform,Sphere,Box,Square,Cylinder,Cone"`
+	Transform    transform `title:"Tranform of the object"`
+	Material     string    `title:"Name of the material to use"`
+	TopRadius    float64   `title:"Top radius for cone objects"`
+	BottomRadius float64   `title:"Bottom radius for cone objects"`
+	Capped       bool      `title:"Whether to cap the ends of cones/cylinders"`
+	Children     []object  `title:"Child objects that inherit this one's transform"`
 }
 
-// type Enum interface {
-// 	EnumVals() []int
-// }
-
-// type Background int
-
-// const (
-// 	Uniform Background = iota
-// 	Skybox
-// )
-
-// func (b Background) String() string {
-// 	switch b {
-// 	case Uniform:
-// 		return "Uniform"
-// 	case Skybox:
-// 		return "Skybox"
-// 	}
-// 	return ""
-// }
-
-// func (b Background) EnumVals() []int {
-// 	return []int{int(Uniform), int(Skybox)}
-// }
-
-// Angle is in degrees.
-type Angle struct {
-	Degrees float64 `min:"0.0" max:"360.0"`
+type transform struct {
+	Translate   vector  `title:"Translation"`
+	RotateAxis  vector  `title:"Axis to rotate around"`
+	RotateAngle float64 `title:"Angle to rotate around the axis in degrees" min:"-360.0" max:"360.0"`
+	Scale       vector  `title:"Scale"`
 }
 
-// Color is an RGB color.
-type Color struct {
+type color struct {
 	R, G, B float64 `min:"0.0" max:"1.0" default:"0.0"`
 }
 
-// Vector is a 3D vector in space.
-type Vector struct {
+type vector struct {
 	X, Y, Z float64 `min:"-999" max:"999" default:"0.0"`
 }
 
-// Path is a URL or file path.
-// type Path string
-
 // NewOptions returns an intialized Options.
 func NewOptions() *Options {
-	opts := &Options{}
-	// w, _ := reflect.TypeOf(opts.Resolution).FieldByName("W")
-	// opts.Resolution.W, _ = strconv.Atoi(w.Tag.Get("default"))
-	// h, _ := reflect.TypeOf(opts.Resolution).FieldByName("H")
-	// opts.Resolution.H, _ = strconv.Atoi(h.Tag.Get("default"))
-
-	// p, _ := reflect.TypeOf(opts.Camera).FieldByName("Position")
-	// opts.Camera.Position.X, _ = strconv.Atoi(p.Tag.Get("default"))
-	// Camera struct {
-	// 	Position Vector  `desc:"Camera position"`
-	// 	LookAt   Vector  `desc:"The point that the camera is looking at"`
-	// 	UpDir    Vector  `desc:"The up direction"`
-	// 	Fov      Degrees `desc:"Field of view" min:"1" max:"180" default:"53"`
-	// } `desc:"Camera position and orientation"`
-	opts.Materials = append(opts.Materials, &Material{Name: "abc"})
-	opts.Materials = append(opts.Materials, &Material{Name: "def"})
+	opts := &Options{
+		Global: global{
+			FastRender:       true,
+			MaxRecursion:     2,
+			SoftShadowDetail: 0,
+		},
+		Resolution: resolution{600, 600},
+		Camera: camera{
+			Position: vector{0, 5, 10},
+			LookAt:   vector{0, 0, 0},
+			UpDir:    vector{0, 1, 0},
+			Fov:      58,
+		},
+		Background: background{
+			Type:  "Uniform",
+			Color: color{0, 0, 0},
+			Image: "",
+		},
+		AntiAlias: antiAlias{
+			MaxDivisions: 0,
+			Threshold:    0,
+		},
+		Lights: []*light{
+			{
+				Type:      "Directional",
+				Color:     color{1, 1, 1},
+				Direction: vector{-1, -1, -1},
+			},
+		},
+		Materials: []*material{
+			{
+				Name:         "white",
+				Emissive:     matProperty{Type: "Uniform", Color: color{0, 0, 0}},
+				Ambient:      matProperty{Type: "Uniform", Color: color{0.3, 0.3, 0.3}},
+				Diffuse:      matProperty{Type: "Uniform", Color: color{1, 1, 1}},
+				Specular:     matProperty{Type: "Uniform", Color: color{1, 1, 1}},
+				Reflective:   matProperty{Type: "Uniform", Color: color{0, 0, 0}},
+				Transmissive: matProperty{Type: "Uniform", Color: color{0, 0, 0}},
+				Smoothness:   matProperty{Type: "Uniform", Color: color{0.9, 0.9, 0.9}},
+				Index:        1,
+				Normal:       "",
+				IsLiquid:     false,
+				Brdf:         "Blinn-Phong",
+			},
+		},
+		Objects: []*object{
+			{
+				Type: "Square",
+				Transform: transform{
+					Translate:   vector{0, 0, 0},
+					RotateAxis:  vector{1, 0, 0},
+					RotateAngle: -90,
+					Scale:       vector{10, 10, 0},
+				},
+				Material: "white",
+			},
+			{
+				Type: "Box",
+				Transform: transform{
+					Translate:   vector{0, 1, 0},
+					RotateAxis:  vector{0, 0, 0},
+					RotateAngle: 0,
+					Scale:       vector{2, 2, 2},
+				},
+				Material: "white",
+			},
+		},
+	}
 	return opts
 }
--- webtracer/js/script.go
+++ webtracer/js/script.go
@@ -34,7 +34,6 @@ func onBodyLoad() {
 
 func initCallbacks() {
 	jq(".tab").Call(jquery.CLICK, onToggleControls)
-	jq("#fast-render").Call(jquery.CLICK, onToggleMode)
 	jq("#save").Call(jquery.CLICK, onSave)
 	jq("#load").Call(jquery.CLICK, onLoad)
 	jq("#download").Call(jquery.CLICK, onDownload)
@@ -59,255 +58,38 @@ func initOptions(options *lib.Options) {
 		return
 	}
 	opts.Append(o)
-	// st := reflect.TypeOf(*options)
-	// sv := reflect.ValueOf(options).Elem()
-	// var siblings []optionItem
-	// for i := 0; i < st.NumField(); i++ {
-	// 	o := handleOptionField(st.Field(i), sv.Field(i), &siblings)
-	// 	siblings = append(siblings, optionItem{jq: o, st: st.Field(i)})
-	// 	opts.Append(o)
-	// }
+
+	addOptionSlides(o)
 }
 
-// func handleOptionField(field reflect.StructField, val reflect.Value,
-// 	siblings *[]optionItem) (opts jquery.JQuery) {
-// 	d := field.Tag.Get("default")
-// 	switch field.Type.Kind() {
-// 	case reflect.Struct:
-// 		opts = optionGroup(field.Name)
-// 		g := jq("<div>").AddClass("option-group-options")
-// 		t := field.Type
-// 		var sibs []optionItem
-// 		for i := 0; i < t.NumField(); i++ {
-// 			f := t.Field(i)
-// 			v := val.Field(i)
-// 			o := handleOptionField(f, v, &sibs).SetAttr("title", f.Tag.Get("desc"))
-// 			sibs = append(sibs, optionItem{jq: o, st: f})
-// 			g.Append(o)
-// 		}
-// 		opts.Call(jquery.CLICK, func(event jquery.Event) {
-// 			g.SlideToggle("fast")
-// 			event.StopPropagation()
-// 		})
-// 		opts.Append(g)
-// 	case reflect.Slice:
-// 		console.Call("log", "handle slice", field.Name)
-// 		opts = optionList(field.Name)
-// 		items := jq("<div>").AddClass("option-list-items")
-// 		console.Call("log", "val.Len", val.Len())
-// 		for i := 0; i < val.Len(); i++ {
-// 			console.Call("log", "i", i)
-// 			// item := jq("<div>").AddClass("option-list-item")
-// 			item := optionGroup(string(byte('0') + byte(i)))
-// 			g := jq("<div>").AddClass("option-group-options")
-// 			sv := val.Index(i)
-// 			console.Call("log", "sv", sv)
-// 			// console.Call("log", "sv", sv.Elem().Field(0))
-// 			st := sv.Elem().Type()
-// 			console.Call("log", "st", st.Name())
-// 			console.Call("log", "st fields", st.NumField())
-// 			var sibs []optionItem
-// 			for i := 0; i < st.NumField(); i++ {
-// 				f := st.Field(i)
-// 				v := sv.Elem().Field(i)
-// 				o := handleOptionField(f, v, &sibs).SetAttr("title", f.Tag.Get("desc"))
-// 				sibs = append(sibs, optionItem{jq: o, st: f})
-// 				g.Append(o)
-// 			}
-// 			item.Call(jquery.CLICK, func(event jquery.Event) {
-// 				g.SlideToggle("fast")
-// 				event.StopPropagation()
-// 			})
-// 			item.Append(g)
-// 			console.Call("log", item)
-// 			items.Append(item)
-// 		}
-// 		opts.Call(jquery.CLICK, func(event jquery.Event) {
-// 			items.SlideToggle("fast")
-// 			event.StopPropagation()
-// 		})
-// 		opts.Append(items)
-// 	case reflect.Int:
-// 		opts = numberOption(field.Name, field.Tag.Get("min"), field.Tag.Get("max"), d)
-// 		opts.Call(jquery.CHANGE, func(event jquery.Event) {
-// 			i, e := strconv.Atoi(event.Target.Get("value").String())
-// 			if e != nil {
-// 				panic(e)
-// 			}
-// 			val.SetInt(int64(i))
-// 		})
-// 		if d == "" {
-// 			d = "0"
-// 		}
-// 		i, e := strconv.Atoi(d)
-// 		if e != nil {
-// 			panic(e)
-// 		}
-// 		val.SetInt(int64(i))
-// 	case reflect.Float64:
-// 		opts = numberOption(field.Name, field.Tag.Get("min"), field.Tag.Get("max"), d)
-// 		opts.Call(jquery.CHANGE, func(event jquery.Event) {
-// 			i, e := strconv.ParseFloat(event.Target.Get("value").String(), 64)
-// 			if e != nil {
-// 				panic(e)
-// 			}
-// 			val.SetFloat(i)
-// 		})
-// 		if d == "" {
-// 			d = "0.0"
-// 		}
-// 		i, e := strconv.ParseFloat(d, 64)
-// 		if e != nil {
-// 			panic(e)
-// 		}
-// 		val.SetFloat(i)
-// 	case reflect.String:
-// 		if field.Name == "Type" {
-// 			opts = choiceOption(field.Name, strings.Split(field.Tag.Get("Types"), ","))
-// 			opts.Call(jquery.CHANGE, func(event jquery.Event) {
-// 				val := event.Target.Get("value").String()
-// 			outer:
-// 				for _, s := range *siblings {
-// 					if s.st.Name != field.Name {
-// 						for _, t := range strings.Split(s.st.Tag.Get("Type"), ",") {
-// 							if t == val {
-// 								s.jq.SlideDown("fast")
-// 								continue outer
-// 							}
-// 						}
-// 						s.jq.SlideUp("fast")
-// 					}
-// 				}
-// 			})
-// 		} else {
-// 			opts = textOption(field.Name)
-// 		}
-// 		val.SetString(d)
-// 	default:
-// 		console.Call("log", "is other")
-// 		opts = numberOption(field.Name, "1", "1", "1")
-// 	}
-// 	opts.SetAttr("title", field.Tag.Get("desc"))
-// outer:
-// 	for _, s := range *siblings {
-// 		if s.st.Name == "Type" {
-// 			for _, t := range strings.Split(field.Tag.Get("Type"), ",") {
-// 				if t == s.jq.Children("select").First().Val() {
-// 					opts.Show()
-// 					break outer
-// 				}
-// 			}
-// 			opts.Hide()
-// 		}
-// 	}
-// 	return
-// }
+func addOptionSlides(opts jquery.JQuery) {
+	structs := jq(".go-struct-field > .go-struct")
+	structs.Each(func(i int, intf interface{}) {
+		obj := intf.(*js.Object)
+		label := jq(obj.Get("parentNode").Get("children").Index(0))
+		st := jq(obj)
+		jq(label).Call(jquery.CLICK, func(event jquery.Event) {
+			st.SlideToggle("fast")
+			event.StopPropagation()
+		})
+	})
 
-// func optionGroup(title string) jquery.JQuery {
-// 	g := jq("<div>").AddClass("option-group")
-// 	h := jq("<div>").AddClass("option-group-header").SetText(title)
-// 	return g.Append(h)
-// }
-
-// func optionList(title string) jquery.JQuery {
-// 	g := jq("<div>").AddClass("option-list")
-// 	h := jq("<div>").AddClass("option-group-header").SetText(title)
-// 	return g.Append(h)
-// }
-
-// func numberOption(label, min, max, value string) jquery.JQuery {
-// 	o := option(label)
-// 	in := jq("<input>").SetAttr("type", "number").SetAttr("min", min).SetAttr("max", max).SetAttr("value", value)
-// 	return o.Append(in)
-// }
-
-// func textOption(label string) jquery.JQuery {
-// 	o := option(label)
-// 	in := jq("<input>").SetAttr("type", "text")
-// 	return o.Append(in)
-// }
-
-// func choiceOption(label string, choices []string) jquery.JQuery {
-// 	o := option(label)
-// 	s := jq("<select>")
-// 	for _, c := range choices {
-// 		s.Append(jq("<option>").SetAttr("value", c).SetText(c))
-// 	}
-// 	return o.Append(s)
-// }
-
-// func option(label string) jquery.JQuery {
-// 	o := jq("<div>").AddClass("option")
-// 	o.Append(label + " ")
-// 	o.Call(jquery.CLICK, func(event jquery.Event) {
-// 		event.StopPropagation()
-// 	})
-// 	return o
-// }
-
-// func vectorOption() jquery.JQuery {
-// 	return jq("<span>") //.Append(numberOption("x", -999, 999, 0)).Append(numberOption("y", -999, 999, 0)).
-// 	//	Append(numberOption("z", -999, 999, 0))
-// }
-
-// func initOptions() {
-// 	opts := jq("#options")
-// 	// initDebugOptions(opts)
-// 	g := globalOptionGroup()
-// 	opts.Append(g)
-// }
-
-// func globalOptionGroup() jquery.JQuery {
-// 	global := optionGroup("Global")
-// 	g := subOptionGroup("Resolution")
-// 	g.Append(numberOption("w", 1, 16000, 800))
-// 	g.Append(numberOption("h", 1, 16000, 600))
-// 	global.Append(g)
-// 	g = subOptionGroup("Camera Position")
-// 	g.Append(vectorOption())
-// 	global.Append(g)
-// 	g = subOptionGroup("Camera LookAt")
-// 	g.Append(vectorOption())
-// 	global.Append(g)
-// 	g = subOptionGroup("Camera Up Direction")
-// 	g.Append(vectorOption())
-// 	global.Append(g)
-// 	g = subOptionGroup("Camera FOV")
-// 	g.Append(numberOption("angle", 1, 180, 53))
-// 	global.Append(g)
-// 	g = subOptionGroup("Background")
-// 	// g.Append(colorOption())
-// 	global.Append(g)
-// 	return global
-// }
-
-// func subOptionGroup(label string) jquery.JQuery {
-// 	l := jq("<div>").AddClass("suboption-label").SetText(label)
-// 	return jq("<div>").AddClass("suboption-group").Append(l)
-// }
+	slices := jq(".go-struct-field > .go-slice")
+	slices.Each(func(i int, intf interface{}) {
+		obj := intf.(*js.Object)
+		label := jq(obj.Get("parentNode").Get("children").Index(0))
+		st := jq(obj)
+		jq(label).Call(jquery.CLICK, func(event jquery.Event) {
+			st.SlideToggle("fast")
+			event.StopPropagation()
+		})
+	})
+}
 
 func onToggleControls() {
-	console.Call("log", "toggle")
-	c := jq(".controls")
-	if c.HasClass("open") {
-		c.RemoveClass("open")
-		c.AddClass("closed")
-	} else {
-		c.RemoveClass("closed")
-		c.AddClass("open")
-	}
-}
-
-func onToggleMode() {
-	btn := jq("#fast-render")
-	switch {
-	case btn.HasClass("toggle-button-down"):
-		btn.RemoveClass("toggle-button-down")
-		btn.AddClass("toggle-button-up")
-	case btn.HasClass("toggle-button-up"):
-		btn.RemoveClass("toggle-button-up")
-		btn.AddClass("toggle-button-down")
-	}
+	jq("#options").SlideToggle("fast")
+	jq("#tab-up").Toggle()
+	jq("#tab-down").Toggle()
 }
 
 func onSave() {
