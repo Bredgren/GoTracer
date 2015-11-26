75340a961a4ae75e8bae60f515e91eefd8dbde47
Bredgren <brandon.edgren@gmail.com>
2015-11-25 21:45:49 -0800

    Start using gohtmlctrl.

--- lib/options.go
+++ lib/options.go
@@ -6,16 +6,15 @@ type Options struct {
 		W, H int `min:"1" max:"16000" default:"600"`
 	} `desc:"Image width and height in pixels"`
 	Camera struct {
-		Position Vector  `desc:"Camera position"`
-		LookAt   Vector  `desc:"The point that the camera is looking at"`
-		UpDir    Vector  `desc:"The up direction"`
-		Fov      Degrees `desc:"Field of view" min:"1" max:"180" default:"53"`
+		Position Vector `desc:"Camera position"`
+		LookAt   Vector `desc:"The point that the camera is looking at"`
+		UpDir    Vector `desc:"The up direction"`
+		Fov      Angle  `desc:"Field of view"`
 	} `desc:"Camera position and orientation"`
 	Background struct {
-		// Type  Background `desc:"Type of background" default:"Uniform"`
 		Type  string `desc:"Type of background" Types:"Uniform,Skybox" default:"Uniform"`
 		Color Color  `desc:"Color to use for a solid background" Type:"Uniform"`
-		Image Path   `desc:"Path/URL of image to use as skybox" Type:"Skybox"`
+		Image string `desc:"Path/URL of image to use as skybox" Type:"Skybox"`
 	} `desc:"Determines the color when a ray doesn't hit anything"`
 	AntiAlias struct {
 		MaxDivisions int     `desc:"Maximum subdivisions of a pixel" min:"0" max:"16" default:"0"`
@@ -30,7 +29,7 @@ type Options struct {
 
 // Light is a light source in the scene.
 type Light struct {
-	Type      string `desc:"Directional, Point, Spot"`
+	Type      string `desc:"Type of light" Types:"Directional,Point,Spot"`
 	Color     Color  `desc:"Color of the light"`
 	Position  Vector `desc:"Position of the light"`
 	Direction Vector `desc:"Direction of the light" Type:"Directional,Spot"`
@@ -46,27 +45,27 @@ type Light struct {
 
 // Material is a description of a material that can be used by Objects.
 type Material struct {
-	Name         string      `desc:"Unique name for the material" default:"name"`
-	Parent       string      `desc:"Name of existing material to inherit attributes from" default:"parent"`
-	Emissive     interface{} `desc:"Emissive color of the material" type:"Path,Color"`
-	Ambient      interface{} `desc:"Abmient color of the material" type:"Path,Color"`
-	Diffuse      interface{} `desc:"Diffuse color of the material" type:"Path,Color"`
-	Specular     interface{} `desc:"Specular color of the material" type:"Path,Color"`
-	Reflective   interface{} `desc:"Reflectivness of the material" type:"Path,Color"`
-	Transmissive interface{} `desc:"Transmissivness of the material" type:"Path,Color"`
-	Smoothness   interface{} `desc:"Smoothness of the material. Affects size of speclar spots" type:"Path,float64"`
+	Name         string      `desc:"Unique name for the material"`
+	Parent       string      `desc:"Name of existing material to inherit attributes from"`
+	Emissive     MatProperty `desc:"Emissive color of the material"`
+	Ambient      MatProperty `desc:"Abmient color of the material"`
+	Diffuse      MatProperty `desc:"Diffuse color of the material"`
+	Specular     MatProperty `desc:"Specular color of the material"`
+	Reflective   MatProperty `desc:"Reflectivness of the material"`
+	Transmissive MatProperty `desc:"Transmissivness of the material"`
+	Smoothness   MatProperty `desc:"Smoothness of the material. Affects size of speclar spots"`
 	Index        float64     `desc:"Refractive index of the material"`
-	Normal       Path        `desc:"Path/URL of image to use as a normal map"`
+	Normal       string      `desc:"Path/URL of image to use as a normal map"`
 	IsLiquid     bool        `desc:"Overlapping behavior is only defined for non-liquids inside liquids"`
 	Brdf         string      `desc:"Lambert, Blinn-Phong"`
 }
 
 // Object is a rendreable object.
 type Object struct {
-	Type         string   `desc:"Transform, Sphere, Box, Square, Cylinder, Cone"`
+	Type         string   `desc:"Type of shape" Types:"Transform,Sphere,Box,Square,Cylinder,Cone"`
 	Translate    Vector   `desc:"Transform translation" Type:"Transform"`
 	RotateAxis   Vector   `desc:"Transform axis to rotate around" Type:"Transform"`
-	RotateAngle  Degrees  `desc:"Transform angle to rotate around the axis" Type:"Transform"`
+	RotateAngle  Angle    `desc:"Transform angle to rotate around the axis" Type:"Transform"`
 	Scale        Vector   `desc:"Transform scale" Type:"Transform"`
 	SubObjects   []Object `desc:"Objects to apply the transform to" Type:"Transform"`
 	Material     string   `desc:"Name of the material to use" Type:"Sphere,Box,Square,Cylinder,Cone"`
@@ -75,33 +74,42 @@ type Object struct {
 	Capped       bool     `desc:"Whether to cap the cone/cylinder" Type:"Cynlinder,Cone"`
 }
 
-type Enum interface {
-	EnumVals() []int
+// MatProperty is a Material property which can either be a texture or a uniform color
+type MatProperty struct {
+	Type    string `desc:"Type of material" Types:"Uniform,Texture"`
+	Texture string `desc:"Path to texture file" Type:"Texture"`
+	Color   Color  `desc:"Uniform color" Type:"Uniform"`
 }
 
-type Background int
+// type Enum interface {
+// 	EnumVals() []int
+// }
 
-const (
-	Uniform Background = iota
-	Skybox
-)
+// type Background int
 
-func (b Background) String() string {
-	switch b {
-	case Uniform:
-		return "Uniform"
-	case Skybox:
-		return "Skybox"
-	}
-	return ""
+// const (
+// 	Uniform Background = iota
+// 	Skybox
+// )
+
+// func (b Background) String() string {
+// 	switch b {
+// 	case Uniform:
+// 		return "Uniform"
+// 	case Skybox:
+// 		return "Skybox"
+// 	}
+// 	return ""
+// }
+
+// func (b Background) EnumVals() []int {
+// 	return []int{int(Uniform), int(Skybox)}
+// }
+
+// Angle is in degrees.
+type Angle struct {
+	Degrees float64 `min:"0.0" max:"360.0"`
 }
-
-func (b Background) EnumVals() []int {
-	return []int{int(Uniform), int(Skybox)}
-}
-
-// Degrees (as in angle).
-type Degrees float64
 
 // Color is an RGB color.
 type Color struct {
@@ -114,13 +122,25 @@ type Vector struct {
 }
 
 // Path is a URL or file path.
-type Path string
+// type Path string
 
 // NewOptions returns an intialized Options.
 func NewOptions() *Options {
 	opts := &Options{}
-	opts.Resolution.W = 800
-	opts.Resolution.H = 600
-	opts.Materials = append(opts.Materials, &Material{})
+	// w, _ := reflect.TypeOf(opts.Resolution).FieldByName("W")
+	// opts.Resolution.W, _ = strconv.Atoi(w.Tag.Get("default"))
+	// h, _ := reflect.TypeOf(opts.Resolution).FieldByName("H")
+	// opts.Resolution.H, _ = strconv.Atoi(h.Tag.Get("default"))
+
+	// p, _ := reflect.TypeOf(opts.Camera).FieldByName("Position")
+	// opts.Camera.Position.X, _ = strconv.Atoi(p.Tag.Get("default"))
+	// Camera struct {
+	// 	Position Vector  `desc:"Camera position"`
+	// 	LookAt   Vector  `desc:"The point that the camera is looking at"`
+	// 	UpDir    Vector  `desc:"The up direction"`
+	// 	Fov      Degrees `desc:"Field of view" min:"1" max:"180" default:"53"`
+	// } `desc:"Camera position and orientation"`
+	opts.Materials = append(opts.Materials, &Material{Name: "abc"})
+	opts.Materials = append(opts.Materials, &Material{Name: "def"})
 	return opts
 }
--- webtracer/js/script.go
+++ webtracer/js/script.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"reflect"
-	"strconv"
-	"strings"
 
+	"github.com/Bredgren/gohtmlctrl/htmlctrl"
 	"github.com/Bredgren/gotracer/lib"
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/gopherjs/jquery"
@@ -21,12 +20,13 @@ func onBodyLoad() {
 	initCallbacks()
 	options := lib.NewOptions()
 	initOptions(options)
+	console.Call("log", options)
 
 	imgCon := jq(".image-container")
-	console.Call("log", imgCon)
 	img := js.Global.Get("Image").New()
 	img.Set("onload", func() {
 		imgCon.Append(img)
+		console.Call("log", img.Get("width"), img.Get("height"))
 		setImageSize(img.Get("width"), img.Get("height"))
 	})
 	img.Set("src", "/img/render542.png")
@@ -38,10 +38,6 @@ func initCallbacks() {
 	jq("#save").Call(jquery.CLICK, onSave)
 	jq("#load").Call(jquery.CLICK, onLoad)
 	jq("#download").Call(jquery.CLICK, onDownload)
-	jq(".option-group").Call(jquery.CLICK, func(o *js.Object) {
-		// j.SlideToggle()
-		console.Call("log", o)
-	})
 }
 
 func setImageSize(w, h *js.Object) {
@@ -50,124 +46,209 @@ func setImageSize(w, h *js.Object) {
 	imgCon.SetCss("height", h)
 }
 
+type optionItem struct {
+	jq jquery.JQuery
+	st reflect.StructField
+}
+
 func initOptions(options *lib.Options) {
 	opts := jq("#options")
-	console.Call("log", opts)
-	console.Call("log", options)
-	st := reflect.TypeOf(*options)
-	sv := reflect.ValueOf(options).Elem()
-	for i := 0; i < st.NumField(); i++ {
-		opts.Append(handleOptionField(st.Field(i), sv.Field(i)))
+	o, e := htmlctrl.Struct(options, "Options", "all-options", "")
+	if e != nil {
+		console.Call("error", e.Error())
+		return
 	}
+	opts.Append(o)
+	// st := reflect.TypeOf(*options)
+	// sv := reflect.ValueOf(options).Elem()
+	// var siblings []optionItem
+	// for i := 0; i < st.NumField(); i++ {
+	// 	o := handleOptionField(st.Field(i), sv.Field(i), &siblings)
+	// 	siblings = append(siblings, optionItem{jq: o, st: st.Field(i)})
+	// 	opts.Append(o)
+	// }
 }
 
-func handleOptionField(field reflect.StructField, val reflect.Value) (opts jquery.JQuery) {
-	switch field.Type.Kind() {
-	case reflect.Struct:
-		opts = optionGroup(field.Name)
-		g := jq("<div>").AddClass("option-group-options")
-		t := field.Type
-		for i := 0; i < t.NumField(); i++ {
-			f := t.Field(i)
-			v := val.Field(i)
-			g.Append(handleOptionField(f, v).SetAttr("title", f.Tag.Get("desc")))
-		}
-		opts.Call(jquery.CLICK, func(event jquery.Event) {
-			g.SlideToggle("fast")
-			event.StopPropagation()
-		})
-		opts.Append(g)
-	case reflect.Slice:
-		console.Call("log", "is slice")
-		opts = vectorOption()
-	case reflect.Int, reflect.Float64:
-		if field.Type.Implements(reflect.TypeOf((*lib.Enum)(nil)).Elem()) {
-			console.Call("log", field.Type.Name(), "is enum")
-			// opts = choiceOption(field.Name)
-		} else {
-			opts = numberOption(field.Name, field.Tag.Get("min"), field.Tag.Get("max"), field.Tag.Get("default"))
-			opts.Call(jquery.CHANGE, func(event jquery.Event) {
-				if field.Type.Kind() == reflect.Int {
-					i, e := strconv.Atoi(event.Target.Get("value").String())
-					if e != nil {
-						panic(e)
-					}
-					val.SetInt(int64(i))
-				} else {
-					i, e := strconv.ParseFloat(event.Target.Get("value").String(), 64)
-					if e != nil {
-						panic(e)
-					}
-					val.SetFloat(i)
-				}
-			})
-		}
-	case reflect.String:
-		if field.Name == "Type" {
-			opts = choiceOption(field.Name, strings.Split(field.Tag.Get("Types"), ","))
-			opts.Call(jquery.CHANGE, func(event jquery.Event) {
-				console.Call("log", event.Target.Get("value").String())
-			})
-		} else {
-			opts = textOption(field.Name)
-		}
-	default:
-		console.Call("log", "is other")
-		opts = numberOption(field.Name, "1", "1", "1")
-	}
-	opts.SetAttr("title", field.Tag.Get("desc"))
-	return
-}
-
-// type group struct {
+// func handleOptionField(field reflect.StructField, val reflect.Value,
+// 	siblings *[]optionItem) (opts jquery.JQuery) {
+// 	d := field.Tag.Get("default")
+// 	switch field.Type.Kind() {
+// 	case reflect.Struct:
+// 		opts = optionGroup(field.Name)
+// 		g := jq("<div>").AddClass("option-group-options")
+// 		t := field.Type
+// 		var sibs []optionItem
+// 		for i := 0; i < t.NumField(); i++ {
+// 			f := t.Field(i)
+// 			v := val.Field(i)
+// 			o := handleOptionField(f, v, &sibs).SetAttr("title", f.Tag.Get("desc"))
+// 			sibs = append(sibs, optionItem{jq: o, st: f})
+// 			g.Append(o)
+// 		}
+// 		opts.Call(jquery.CLICK, func(event jquery.Event) {
+// 			g.SlideToggle("fast")
+// 			event.StopPropagation()
+// 		})
+// 		opts.Append(g)
+// 	case reflect.Slice:
+// 		console.Call("log", "handle slice", field.Name)
+// 		opts = optionList(field.Name)
+// 		items := jq("<div>").AddClass("option-list-items")
+// 		console.Call("log", "val.Len", val.Len())
+// 		for i := 0; i < val.Len(); i++ {
+// 			console.Call("log", "i", i)
+// 			// item := jq("<div>").AddClass("option-list-item")
+// 			item := optionGroup(string(byte('0') + byte(i)))
+// 			g := jq("<div>").AddClass("option-group-options")
+// 			sv := val.Index(i)
+// 			console.Call("log", "sv", sv)
+// 			// console.Call("log", "sv", sv.Elem().Field(0))
+// 			st := sv.Elem().Type()
+// 			console.Call("log", "st", st.Name())
+// 			console.Call("log", "st fields", st.NumField())
+// 			var sibs []optionItem
+// 			for i := 0; i < st.NumField(); i++ {
+// 				f := st.Field(i)
+// 				v := sv.Elem().Field(i)
+// 				o := handleOptionField(f, v, &sibs).SetAttr("title", f.Tag.Get("desc"))
+// 				sibs = append(sibs, optionItem{jq: o, st: f})
+// 				g.Append(o)
+// 			}
+// 			item.Call(jquery.CLICK, func(event jquery.Event) {
+// 				g.SlideToggle("fast")
+// 				event.StopPropagation()
+// 			})
+// 			item.Append(g)
+// 			console.Call("log", item)
+// 			items.Append(item)
+// 		}
+// 		opts.Call(jquery.CLICK, func(event jquery.Event) {
+// 			items.SlideToggle("fast")
+// 			event.StopPropagation()
+// 		})
+// 		opts.Append(items)
+// 	case reflect.Int:
+// 		opts = numberOption(field.Name, field.Tag.Get("min"), field.Tag.Get("max"), d)
+// 		opts.Call(jquery.CHANGE, func(event jquery.Event) {
+// 			i, e := strconv.Atoi(event.Target.Get("value").String())
+// 			if e != nil {
+// 				panic(e)
+// 			}
+// 			val.SetInt(int64(i))
+// 		})
+// 		if d == "" {
+// 			d = "0"
+// 		}
+// 		i, e := strconv.Atoi(d)
+// 		if e != nil {
+// 			panic(e)
+// 		}
+// 		val.SetInt(int64(i))
+// 	case reflect.Float64:
+// 		opts = numberOption(field.Name, field.Tag.Get("min"), field.Tag.Get("max"), d)
+// 		opts.Call(jquery.CHANGE, func(event jquery.Event) {
+// 			i, e := strconv.ParseFloat(event.Target.Get("value").String(), 64)
+// 			if e != nil {
+// 				panic(e)
+// 			}
+// 			val.SetFloat(i)
+// 		})
+// 		if d == "" {
+// 			d = "0.0"
+// 		}
+// 		i, e := strconv.ParseFloat(d, 64)
+// 		if e != nil {
+// 			panic(e)
+// 		}
+// 		val.SetFloat(i)
+// 	case reflect.String:
+// 		if field.Name == "Type" {
+// 			opts = choiceOption(field.Name, strings.Split(field.Tag.Get("Types"), ","))
+// 			opts.Call(jquery.CHANGE, func(event jquery.Event) {
+// 				val := event.Target.Get("value").String()
+// 			outer:
+// 				for _, s := range *siblings {
+// 					if s.st.Name != field.Name {
+// 						for _, t := range strings.Split(s.st.Tag.Get("Type"), ",") {
+// 							if t == val {
+// 								s.jq.SlideDown("fast")
+// 								continue outer
+// 							}
+// 						}
+// 						s.jq.SlideUp("fast")
+// 					}
+// 				}
+// 			})
+// 		} else {
+// 			opts = textOption(field.Name)
+// 		}
+// 		val.SetString(d)
+// 	default:
+// 		console.Call("log", "is other")
+// 		opts = numberOption(field.Name, "1", "1", "1")
+// 	}
+// 	opts.SetAttr("title", field.Tag.Get("desc"))
+// outer:
+// 	for _, s := range *siblings {
+// 		if s.st.Name == "Type" {
+// 			for _, t := range strings.Split(field.Tag.Get("Type"), ",") {
+// 				if t == s.jq.Children("select").First().Val() {
+// 					opts.Show()
+// 					break outer
+// 				}
+// 			}
+// 			opts.Hide()
+// 		}
+// 	}
+// 	return
 // }
 
-// type gOption struct {
-// 	structField
-// 	uiField
-// 	parentGroup
+// func optionGroup(title string) jquery.JQuery {
+// 	g := jq("<div>").AddClass("option-group")
+// 	h := jq("<div>").AddClass("option-group-header").SetText(title)
+// 	return g.Append(h)
 // }
 
-func optionGroup(title string) jquery.JQuery {
-	g := jq("<div>").AddClass("option-group")
-	h := jq("<div>").AddClass("option-group-header").SetText(title)
-	return g.Append(h)
-}
+// func optionList(title string) jquery.JQuery {
+// 	g := jq("<div>").AddClass("option-list")
+// 	h := jq("<div>").AddClass("option-group-header").SetText(title)
+// 	return g.Append(h)
+// }
 
-func numberOption(label, min, max, value string) jquery.JQuery {
-	o := option(label)
-	in := jq("<input>").SetAttr("type", "number").SetAttr("min", min).SetAttr("max", max).SetAttr("value", value)
-	return o.Append(in)
-}
+// func numberOption(label, min, max, value string) jquery.JQuery {
+// 	o := option(label)
+// 	in := jq("<input>").SetAttr("type", "number").SetAttr("min", min).SetAttr("max", max).SetAttr("value", value)
+// 	return o.Append(in)
+// }
 
-func textOption(label string) jquery.JQuery {
-	o := option(label)
-	in := jq("<input>").SetAttr("type", "text")
-	return o.Append(in)
-}
+// func textOption(label string) jquery.JQuery {
+// 	o := option(label)
+// 	in := jq("<input>").SetAttr("type", "text")
+// 	return o.Append(in)
+// }
 
-func choiceOption(label string, choices []string) jquery.JQuery {
-	o := option(label)
-	s := jq("<select>")
-	for _, c := range choices {
-		s.Append(jq("<option>").SetText(c))
-	}
-	return o.Append(s)
-}
+// func choiceOption(label string, choices []string) jquery.JQuery {
+// 	o := option(label)
+// 	s := jq("<select>")
+// 	for _, c := range choices {
+// 		s.Append(jq("<option>").SetAttr("value", c).SetText(c))
+// 	}
+// 	return o.Append(s)
+// }
 
-func option(label string) jquery.JQuery {
-	o := jq("<div>").AddClass("option")
-	o.Append(label + " ")
-	o.Call(jquery.CLICK, func(event jquery.Event) {
-		event.StopPropagation()
-	})
-	return o
-}
+// func option(label string) jquery.JQuery {
+// 	o := jq("<div>").AddClass("option")
+// 	o.Append(label + " ")
+// 	o.Call(jquery.CLICK, func(event jquery.Event) {
+// 		event.StopPropagation()
+// 	})
+// 	return o
+// }
 
-func vectorOption() jquery.JQuery {
-	return jq("<span>") //.Append(numberOption("x", -999, 999, 0)).Append(numberOption("y", -999, 999, 0)).
-	//	Append(numberOption("z", -999, 999, 0))
-}
+// func vectorOption() jquery.JQuery {
+// 	return jq("<span>") //.Append(numberOption("x", -999, 999, 0)).Append(numberOption("y", -999, 999, 0)).
+// 	//	Append(numberOption("z", -999, 999, 0))
+// }
 
 // func initOptions() {
 // 	opts := jq("#options")
