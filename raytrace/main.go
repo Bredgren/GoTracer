6f55b4674658ac16f9a5f59b4b85bb06d2f38cb7
Brandon Edgren <brandon.edgren@gmail.com>
2015-11-28 21:03:53 -0800

    Implement most of Trace function

--- raytrace/main.go
+++ raytrace/main.go
@@ -1,6 +1,6 @@
 /*
 The raytracer application takes a scene as a json file and produces a rendered version of that scene as
-a png or jpeg image. See lib.Options for details on all the options.
+a png or jpeg image. See trace.Options for details on all the options.
 */
 package main
 
@@ -15,7 +15,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/Bredgren/gotracer/lib"
 	"github.com/Bredgren/gotracer/trace"
 )
 
@@ -52,22 +51,6 @@ func init() {
 	// flag.BoolVar(&verbose, "v", verbose, "Print progress reports")
 }
 
-// type grid struct {
-// 	scene                  *raytracer.Scene
-// 	xMin, yMin, xMax, yMax int // Min inclusive, Max exclusive
-// 	img                    *image.NRGBA
-// 	ch                     chan float64 // signal done
-// }
-
-// func traceGrid(g grid) {
-// 	for y := g.yMin; y < g.yMax; y++ {
-// 		for x := g.xMin; x < g.xMax; x++ {
-// 			g.img.SetNRGBA(x, y, g.scene.TracePixel(x, y))
-// 		}
-// 	}
-// 	g.ch <- 1
-// }
-
 func main() {
 	flag.Parse()
 
@@ -97,7 +80,7 @@ func main() {
 	}
 
 	// Read and decode options
-	var options lib.Options
+	var options trace.Options
 	jsonOpts, e := ioutil.ReadAll(inReader)
 	if e != nil {
 		log.Fatalf("reading file: %s\n", e)
@@ -107,7 +90,7 @@ func main() {
 		log.Fatalf("decoding json: %s\n", e)
 	}
 
-	img := trace.Trace(options, gridSize)
+	img := trace.Trace(&options, gridSize)
 
 	// Choose stdout or user-specified file
 	var outWriter io.Writer = os.Stdout
@@ -126,112 +109,4 @@ func main() {
 	case "png":
 		png.Encode(outWriter, img)
 	}
-
-	// 	sceneFilePath := sceneDir + "/" + sceneFile
-	// 	scene := raytracer.Parse(sceneFilePath)
-
-	// 	imgW = scene.Camera.ImageWidth
-	// 	imgH = scene.Camera.ImageHeight
-	// 	// rayCounts = make([][]map[int]int, imgW)
-	// 	// for x := 0; x < imgW; x++ {
-	// 	// 	rayCounts[x] = make([]map[int]int, imgH)
-	// 	// 	for y := 0; y < imgH; y++ {
-	// 	// 		rayCounts[x][y] = make(map[int]int)
-	// 	// 	}
-	// 	// }
-	// 	bounds := image.Rect(0, 0, imgW, imgH)
-	// 	img := image.NewNRGBA(bounds)
-
-	// 	gridChan := make(chan float64, 100)
-	// 	gridCount := 0.0
-
-	// 	log.Println("Begin tracing")
-	// 	begin := time.Now()
-	// 	for y := bounds.Min.Y; y < bounds.Max.Y; y += gridSize {
-	// 		for x := bounds.Min.X; x < bounds.Max.X; x += gridSize {
-	// 			xMax := x + gridSize
-	// 			if xMax > imgW {
-	// 				xMax = imgW
-	// 			}
-	// 			yMax := y + gridSize
-	// 			if yMax > imgH {
-	// 				yMax = imgH
-	// 			}
-	// 			go traceGrid(grid{scene, x, y, xMax, yMax, img, gridChan})
-	// 			gridCount++
-	// 		}
-	// 	}
-	// 	for g := 0.0; g < gridCount; g += <-gridChan {
-	// 		log.Printf("%.2f%%", g/gridCount*100)
-	// 	}
-	// 	end := time.Now()
-	// 	log.Printf("Done tracing, took %v", end.Sub(begin))
-
-	// 	if noImg {
-	// 		return
-	// 	}
-
-	// 	files, err := ioutil.ReadDir(renderDir)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	count := 0
-	// 	for _, file := range files {
-	// 		name := file.Name()
-	// 		if strings.HasPrefix(name, "render") && len(name) > 10 &&
-	// 			!strings.Contains(name, "Rays") && !strings.Contains(name, "scene") {
-	// 			number, err := strconv.Atoi(name[6 : len(name)-4])
-	// 			if err != nil {
-	// 				log.Fatal(err)
-	// 			}
-	// 			if number > count {
-	// 				count = number
-	// 			}
-	// 		}
-	// 	}
-
-	// 	baseFileName := fmt.Sprintf("%s/render%d", renderDir, count+1)
-
-	// 	outFile := fmt.Sprintf("%s.%s", baseFileName, format)
-
-	// 	file, err := os.Create(outFile)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer file.Close()
-
-	// 	if format == "jpg" {
-	// 		jpeg.Encode(file, img, &jpeg.Options{jpegQuality})
-	// 	} else if format == "png" {
-	// 		png.Encode(file, img)
-	// 	}
-	// 	log.Printf("Saved %s", outFile)
-
-	// 	copyFileContents(sceneFilePath, baseFileName+"scene.json")
-
-	// 	// saveRayCounts(baseFileName)
-	// }
-
-	// func copyFileContents(src, dst string) (err error) {
-	// 	in, err := os.Open(src)
-	// 	if err != nil {
-	// 		return
-	// 	}
-	// 	defer in.Close()
-	// 	out, err := os.Create(dst)
-	// 	if err != nil {
-	// 		return
-	// 	}
-	// 	defer func() {
-	// 		cerr := out.Close()
-	// 		if err == nil {
-	// 			err = cerr
-	// 		}
-	// 	}()
-	// 	if _, err = io.Copy(out, in); err != nil {
-	// 		return
-	// 	}
-	// 	err = out.Sync()
-	// 	log.Printf("Saved %s", dst)
-	// 	return
 }
--- trace/trace.go
+++ trace/trace.go
@@ -1,9 +1,48 @@
 package trace
 
-import "image"
+import (
+	"fmt"
+	"image"
+	"sync"
+)
 
 // Trace renders an image according to the given options. Renders the image in chuncks of the given
 // size in parallel.
 func Trace(options *Options, gridSize int) *image.NRGBA {
-	return nil
+	// Create scene
+	// ...
+
+	imgW := options.Resolution.W
+	imgH := options.Resolution.H
+	bounds := image.Rect(0, 0, imgW, imgH)
+	img := image.NewNRGBA(bounds)
+
+	gridGroup := sync.WaitGroup{}
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y += gridSize {
+		for x := bounds.Min.X; x < bounds.Max.X; x += gridSize {
+			xMax := x + gridSize
+			if xMax > imgW {
+				xMax = imgW
+			}
+			yMax := y + gridSize
+			if yMax > imgH {
+				yMax = imgH
+			}
+			x, y := x, y
+			gridGroup.Add(1)
+			go func() {
+				for y := y; y < yMax; y++ {
+					for x := x; x < xMax; x++ {
+						fmt.Println(x, y)
+						// img.SetNRGBA(x, y, scene.TracePixel(x, y))
+					}
+				}
+				gridGroup.Done()
+			}()
+		}
+	}
+	gridGroup.Wait()
+
+	return img
 }
