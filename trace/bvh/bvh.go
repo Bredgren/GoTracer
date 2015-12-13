d9c1442456f364884a93bcafa1c09f844725df50
Brandon Edgren <brandon.edgren@gmail.com>
2015-12-12 16:25:59 -0800

    Implement and test BVH.

--- trace/bvh/bvh.go
+++ trace/bvh/bvh.go
@@ -1,6 +1,9 @@
 package bvh
 
 import (
+	"math"
+	"sort"
+
 	"github.com/Bredgren/gotracer/trace/ray"
 	"github.com/go-gl/mathgl/mgl64"
 )
@@ -8,18 +11,24 @@ import (
 // Node is a node in the BVH tree. They have up to 2 child nodes and an optional Object. Nodes
 // without children are leaves. Non-leaf Nodes are not expected to contain Objcts.
 type Node struct {
-	Right Intersector
-	Left  Intersector
-	AABB
+	Right     *Node
+	Left      *Node
+	Object    Intersector
+	splitAxis int
 }
 
+// Intersector is an object that can be tested for intersection with a ray.
 type Intersector interface {
-	Intersect(*ray.Ray) bool
+	// Intersect must make no assumtions about the initial state of IntersectionResult and
+	// must be sure to set all fields appropriately.
+	Intersect(*ray.Ray, *IntersectResult)
+	AABB() *AABB
 }
 
-// Bounder is an object that provides AABB.
-type Bounder interface {
-	Bounds() AABB
+// IntersectResult holds the results of an intersection.
+type IntersectResult struct {
+	Object Intersector
+	T      int
 }
 
 // AABB is an axis-aligned bounding box.
@@ -28,7 +37,9 @@ type AABB struct {
 	Max mgl64.Vec3
 }
 
-func (a *AABB) Intersect(r *ray.Ray) bool {
+// Intersect implements the Intersect method of the Intersector interface..
+func (a *AABB) Intersect(r *ray.Ray, res *IntersectResult) {
+	res.Object = nil
 	var txmin, txmax, tymin, tymax, tzmin, tzmax float64
 
 	invX := 1 / r.Dir.X()
@@ -50,7 +61,7 @@ func (a *AABB) Intersect(r *ray.Ray) bool {
 	}
 
 	if (txmin > tymax) || (tymin > txmax) {
-		return false
+		return
 	}
 
 	if tymin > txmin {
@@ -70,7 +81,7 @@ func (a *AABB) Intersect(r *ray.Ray) bool {
 	}
 
 	if (txmin > tzmax) || (tzmin > txmax) {
-		return false
+		return
 	}
 
 	if tzmin > txmin {
@@ -80,42 +91,163 @@ func (a *AABB) Intersect(r *ray.Ray) bool {
 		txmax = tzmax
 	}
 
-	return txmin > 0 || txmax > 0
+	if txmin > 0 || txmax > 0 {
+		res.Object = a
+	}
+	return
 }
 
-// NewTree takes a list of Objects and returns the root of a BVH tree.
-func NewTree([]Bounder) *Node {
-	// Note that both children must be non-nil for non-leaf nodes?
-	// Sort objects by position and recursively devide in half
-	return nil
+// AABB implements the AABB method for the Intersector interface.
+func (a *AABB) AABB() *AABB {
+	return a
+}
+
+// Center retuns the center of the AABB
+func (a *AABB) Center() mgl64.Vec3 {
+	return a.Min.Add(a.Max).Mul(0.5)
+}
+
+type intersectorByX []Intersector
+
+func (in intersectorByX) Len() int {
+	return len(in)
+}
+
+func (in intersectorByX) Less(i, j int) bool {
+	centerI := in[i].AABB().Center()
+	centerJ := in[j].AABB().Center()
+	return centerI.X() < centerJ.X()
+}
+
+func (in intersectorByX) Swap(i, j int) {
+	in[i], in[j] = in[j], in[i]
+}
+
+type intersectorByY []Intersector
+
+func (in intersectorByY) Len() int {
+	return len(in)
+}
+
+func (in intersectorByY) Less(i, j int) bool {
+	centerI := in[i].AABB().Center()
+	centerJ := in[j].AABB().Center()
+	return centerI.Y() < centerJ.Y()
+}
+
+func (in intersectorByY) Swap(i, j int) {
+	in[i], in[j] = in[j], in[i]
+}
+
+type intersectorByZ []Intersector
+
+func (in intersectorByZ) Len() int {
+	return len(in)
+}
+
+func (in intersectorByZ) Less(i, j int) bool {
+	centerI := in[i].AABB().Center()
+	centerJ := in[j].AABB().Center()
+	return centerI.Z() < centerJ.Z()
+}
+
+func (in intersectorByZ) Swap(i, j int) {
+	in[i], in[j] = in[j], in[i]
+}
+
+// NewTree takes a list of Intersectors and returns the root of a BVH tree. An empty slice
+// returns nil.
+func NewTree(objects []Intersector) *Node {
+	if len(objects) == 0 {
+		return nil
+	}
+
+	if len(objects) == 1 {
+		return &Node{
+			Left:   nil,
+			Right:  nil,
+			Object: objects[0],
+		}
+	}
+
+	// Get AABB around all objects
+	aabb := AABB{
+		Min: mgl64.Vec3{math.Inf(1), math.Inf(1), math.Inf(1)},
+		Max: mgl64.Vec3{math.Inf(-1), math.Inf(-1), math.Inf(-1)},
+	}
+	for _, o := range objects {
+		ab := o.AABB()
+		for i := 0; i < 3; i++ {
+			if ab.Min[i] < aabb.Min[i] {
+				aabb.Min[i] = ab.Min[i]
+			}
+			if ab.Max[i] > aabb.Max[i] {
+				aabb.Max[i] = ab.Max[i]
+			}
+		}
+	}
+
+	// Sort according to which dimension is largest to try and make things as square as possible
+	width := aabb.Max.X() - aabb.Min.X()
+	height := aabb.Max.Y() - aabb.Min.Y()
+	depth := aabb.Max.Z() - aabb.Min.Z()
+	axis := 0
+	if height > width && height > depth {
+		sort.Sort(intersectorByY(objects))
+		axis = 1
+	} else if depth > width && depth > height {
+		sort.Sort(intersectorByZ(objects))
+		axis = 2
+	} else {
+		sort.Sort(intersectorByX(objects))
+	}
+
+	return &Node{
+		Left:      NewTree(objects[:int(len(objects)/2)]),
+		Right:     NewTree(objects[int(len(objects)/2):]),
+		Object:    &aabb,
+		splitAxis: axis,
+	}
 }
 
 // Intersect takes a ray which be used to find the object that needs to be tested. It does
 // not test intersection with the object itself.
-func (n *Node) Intersect(r *ray.Ray) (hit bool) {
-	// Test self
-	if !n.AABB.Intersect(r) {
-		return false
+func (n *Node) Intersect(r *ray.Ray, res *IntersectResult) {
+	if n == nil {
+		res.Object = nil
+		return
 	}
 
-	// If ray is going right (+X) then start with left group
-	if r.Dir.X() >= 0 {
-		hit = n.Left.Intersect(r)
-		if !hit {
-			hit = n.Right.Intersect(r)
-		}
+	if n.Object.Intersect(r, res); res.Object == nil || (n.Left == nil && n.Right == nil) {
+		// Either we're a node and the ray missed or we're a leaf node, in which case we don't
+		// need to touch res since it already holds the result we want.
+		return
+	}
+
+	// Being careful about res.Object since we're reusing it. At this point it is non-nil
+	// and we are guarenteed to call one of the Intersect methods below which must reset
+	// res.Object appropriately, therefore we don't acutally need to worry about it here.
+
+	var tmpRes IntersectResult
+	// If ray is going in the positive direction then start with left group because that's
+	// the one we're more likely to hit first.
+	if r.Dir[n.splitAxis] >= 0 {
+		n.Left.Intersect(r, res)
+		n.Right.Intersect(r, &tmpRes)
 	} else {
-		hit = n.Right.Intersect(r)
-		if !hit {
-			hit = n.Left.Intersect(r)
-		}
+		n.Right.Intersect(r, res)
+		n.Left.Intersect(r, &tmpRes)
+	}
+
+	// Be sure to pick the non-nil or closest intersection
+	if tmpRes.Object != nil && (res.Object == nil || tmpRes.T < res.T) {
+		*res = tmpRes
 	}
 
 	return
 }
 
-func (n *Node) intersect(r *ray.Ray) int {
-	// Return distance r goes to hit n, or -1 if no intersection
-	// TODO
-	return -1
+// AABB implements the AABB method for the Intersector interface.
+func (n *Node) AABB() *AABB {
+	return n.Object.AABB()
 }
--- trace/bvh/bvh_test.go
+++ trace/bvh/bvh_test.go
@@ -7,26 +7,24 @@ import (
 	"github.com/go-gl/mathgl/mgl64"
 )
 
-// func TestBvh(t *testing.T) {
-// }
-
 func TestAABB(t *testing.T) {
 	aabb := AABB{Min: mgl64.Vec3{1, 0, -2}, Max: mgl64.Vec3{2, 1, 0}}
-	cases := getTestCases(aabb)
+	cases := getTestCases(&aabb)
+	res := IntersectResult{}
 	for i, c := range cases {
-		got := aabb.Intersect(&c.r)
-		if got != c.hit {
-			t.Errorf("case %d: %#v: got=%#v want=%#v", i, c.r, got, c.hit)
+		aabb.Intersect(&c.r, &res)
+		if c.hit != res.Object {
+			t.Errorf("case %d: %#v: got=%#v want=%#v", i, c.r, res.Object, c.hit)
 		}
 	}
 }
 
-var res bool
+var res IntersectResult
 
 func benchmarkAABB(b *testing.B, aabb AABB, r ray.Ray) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		res = aabb.Intersect(&r)
+		aabb.Intersect(&r, &res)
 	}
 }
 
@@ -50,10 +48,10 @@ func BenchmarkAABBHit(b *testing.B) {
 
 type testCase struct {
 	r   ray.Ray
-	hit bool
+	hit Intersector
 }
 
-func getTestCases(aabb AABB) []testCase {
+func getTestCases(aabb *AABB) []testCase {
 	var cases []testCase
 	for i := 0; i < 3; i++ {
 		cases = append(cases, getAxisCases(aabb, i)...)
@@ -61,8 +59,10 @@ func getTestCases(aabb AABB) []testCase {
 	return cases
 }
 
-func getAxisCases(aabb AABB, axis int) []testCase {
+func getAxisCases(aabb *AABB, axis int) []testCase {
 	var cases []testCase
+	// Generates 54 test cases per axis, half for one direction and half for the other.
+	// The 27 per direction are each position around the AABB in a 3x3x3 grid.
 	for x := -1; x < 2; x++ {
 		xCenter := (aabb.Max.X() + aabb.Min.X()) / 2
 		xWidth := (aabb.Max.X() - aabb.Min.X())
@@ -80,22 +80,153 @@ func getAxisCases(aabb AABB, axis int) []testCase {
 				dir1[axis] = 1
 				dir2 := mgl64.Vec3{}
 				dir2[axis] = -1
+				hit1 := Intersector(nil)
+				if (axis == 0 && x <= 0 && y == 0 && z == 0) ||
+					(axis == 1 && x == 0 && y <= 0 && z == 0) ||
+					(axis == 2 && x == 0 && y == 0 && z <= 0) {
+					hit1 = aabb
+				}
+				hit2 := Intersector(nil)
+				if (axis == 0 && x >= 0 && y == 0 && z == 0) ||
+					(axis == 1 && x == 0 && y >= 0 && z == 0) ||
+					(axis == 2 && x == 0 && y == 0 && z >= 0) {
+					hit2 = aabb
+				}
 				cases = append(cases,
 					testCase{
-						r: ray.Ray{Origin: origin, Dir: dir1},
-						hit: (axis == 0 && x <= 0 && y == 0 && z == 0) ||
-							(axis == 1 && x == 0 && y <= 0 && z == 0) ||
-							(axis == 2 && x == 0 && y == 0 && z <= 0),
+						r:   ray.Ray{Origin: origin, Dir: dir1},
+						hit: hit1,
 					},
 					testCase{
-						r: ray.Ray{Origin: origin, Dir: dir2},
-						hit: (axis == 0 && x >= 0 && y == 0 && z == 0) ||
-							(axis == 1 && x == 0 && y >= 0 && z == 0) ||
-							(axis == 2 && x == 0 && y == 0 && z >= 0),
+						r:   ray.Ray{Origin: origin, Dir: dir2},
+						hit: hit2,
 					})
 			}
 		}
 	}
 
 	return cases
+}
+
+func TestBvhConstruction(t *testing.T) {
+	var objects []Intersector
+	tree := NewTree(objects)
+	if tree != nil {
+		t.Errorf("Empty object slice didn't not create nil tree, got this: %#v", tree)
+	}
+
+	o1 := AABB{Min: mgl64.Vec3{0, 0, 0}, Max: mgl64.Vec3{1, 1, 1}}
+	objects = append(objects, &o1)
+	tree = NewTree(objects)
+	if tree.Left != nil || tree.Right != nil || tree.Object != &o1 {
+		t.Errorf("Tree with 1 object: incorrect: %#v", tree)
+	}
+
+	o2 := AABB{Min: mgl64.Vec3{-2, 0, 0}, Max: mgl64.Vec3{-1, 1, 1}}
+	objects = append(objects, &o2)
+	tree = NewTree(objects)
+	if tree.Left == nil {
+		t.Errorf("Tree with 2 objects: Left is nil: %#v", tree)
+	}
+	if tree.Left.Object != &o2 {
+		t.Errorf("Tree with 2 objects: Left is not o2: %#v", tree)
+	}
+	if tree.Right == nil {
+		t.Errorf("Tree with 2 objects: Right is nil: %#v", tree)
+	}
+	if tree.Right.Object != &o1 {
+		t.Errorf("Tree with 2 objects: Left is not o1: %#v", tree)
+	}
+	aabb := tree.AABB()
+	if !aabb.Min.ApproxEqual(o2.Min) && !aabb.Max.ApproxEqual(o1.Max) {
+		t.Errorf("Tree with 2 objects: AABB is incorrect: %#v", tree)
+	}
+
+	o3 := AABB{Min: mgl64.Vec3{0, 0, -1}, Max: mgl64.Vec3{1, 1, 0}}
+	objects = append(objects, &o3)
+	tree = NewTree(objects)
+	if tree.Left == nil {
+		t.Errorf("Tree with 3 objects: Left is nil: %#v", tree)
+	}
+	if tree.Left.Object != &o2 {
+		t.Errorf("Tree with 3 objects: Left is not o2: %#v", tree)
+	}
+	if tree.Right == nil {
+		t.Errorf("Tree with 3 objects: Right is nil: %#v", tree)
+	}
+	if tree.Right.Right.Object != &o1 {
+		t.Errorf("Tree with 3 objects: Right.Right is not o1: %#v", tree)
+	}
+	if tree.Right.Left.Object != &o3 {
+		t.Errorf("Tree with 3 objects: Right.Left is not o3: %#v", tree)
+	}
+	aabb = tree.AABB()
+	if !aabb.Min.ApproxEqual(mgl64.Vec3{-2, 0, -1}) && !aabb.Max.ApproxEqual(o1.Max) {
+		t.Errorf("Tree with 3 objects: AABB is incorrect: %#v", tree)
+	}
+}
+
+func TestBvhIntersect(t *testing.T) {
+	var objects []Intersector
+	tree := NewTree(objects)
+	r1 := ray.Ray{Origin: mgl64.Vec3{0, 0, 5}, Dir: mgl64.Vec3{0, 0, -1}}
+	res := IntersectResult{}
+	tree.Intersect(&r1, &res)
+	if res.Object != nil {
+		t.Errorf("Got hit on empty tree. Object is %#v", res.Object)
+	}
+
+	o1 := AABB{Min: mgl64.Vec3{-1, -1, -1}, Max: mgl64.Vec3{1, 1, 1}}
+	objects = append(objects, &o1)
+	tree = NewTree(objects)
+
+	tree.Intersect(&r1, &res)
+	if res.Object != &o1 {
+		t.Errorf("1 Object: r1 (%#v) didn't hit o1: %#v, tree: %#v", res.Object, r1, tree)
+	}
+
+	r2 := ray.Ray{Origin: mgl64.Vec3{-2, 0, 5}, Dir: mgl64.Vec3{0, 0, -1}}
+	tree.Intersect(&r2, &res)
+	if res.Object == &o1 {
+		t.Errorf("1 Object: r2 (%#v) hit o1: %#v tree: %#v", r2, res.Object, tree)
+	}
+
+	o2 := AABB{Min: mgl64.Vec3{3, -1, -1}, Max: mgl64.Vec3{5, 1, 1}}
+	objects = append(objects, &o2)
+	tree = NewTree(objects)
+
+	tree.Intersect(&r1, &res)
+	if res.Object != &o1 {
+		t.Errorf("2 Objects: r1 (%#v) didn't hit o1: %#v tree: %#v", r1, res.Object, tree)
+	}
+
+	tree.Intersect(&r2, &res)
+	if res.Object != nil {
+		t.Errorf("2 Objects: r2 (%#v) hit something: %#v tree: %#v", r2, res.Object, tree)
+	}
+
+	r3 := ray.Ray{Origin: mgl64.Vec3{4, 0, 5}, Dir: mgl64.Vec3{0, 0, -1}}
+	tree.Intersect(&r3, &res)
+	if res.Object != &o2 {
+		t.Errorf("2 Objects: r3 (%#v) didn't hit o2: hit %#v tree: %#v", r3, res.Object, tree)
+	}
+
+	o3 := AABB{Min: mgl64.Vec3{3, -1, 2}, Max: mgl64.Vec3{5, 1, 4}}
+	objects = append(objects, &o3)
+	tree = NewTree(objects)
+
+	tree.Intersect(&r1, &res)
+	if res.Object != &o1 {
+		t.Errorf("3 Objects: r1 (%#v) didn't hit o1: %#v tree: %#v", r1, res.Object, tree)
+	}
+
+	tree.Intersect(&r2, &res)
+	if res.Object != nil {
+		t.Errorf("3 Objects: r2 (%#v) hit something: %#v tree: %#v", r2, res.Object, tree)
+	}
+
+	tree.Intersect(&r3, &res)
+	if res.Object != &o3 {
+		t.Errorf("3 Objects: r3 (%#v) didn't hit o3: hit %#v tree: %#v", r3, res.Object, tree)
+	}
 }
